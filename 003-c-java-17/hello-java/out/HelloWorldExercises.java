0f735e3c4ebe6d67ef1134f080f4bb3c660767a3
Tech Doll <tdoll@post.com>
2025-04-09 17:59:41 -0400

    Los ejercicios del fichero HelloWorldExercises.java fueron resueltos.

---
+++ 003-c-java-17/hello-java/out/HelloWorldExercises.java
@@ -0,0 +1,56 @@
+public class HelloWorldExercises {
+
+	public static void main(String[] args) {
+		
+		// 1. Imprime un mensaje que diga tu nombre en lugar de "Hola Mundo!".
+		System.out.println("mai t' doll");			// Imprime un mensaje por la Salida Estandar.
+
+		// 2. Imprime dos lineas: "Hola" y luego "Mundo" con un solo println.
+		System.out.println("Hola\nMundo!");			// Imprime un msg, con un salto de linea.
+
+		// 3. Añade un comentario sobre lo que hace cada linea del programa.
+																	// Realizado.
+
+		// 4. Crea un comentario en varias lineas.
+		/*
+		 * Hice
+		 * un
+		 * comentario
+		 * de
+		 * varias
+		 * lineas.
+		 */
+
+		// 5. Imprime tu edad, tu color favorito y tu ciudad.
+		System.out.println("Mi edad es: Veinti-algo.");			// Imprime un msg.
+		System.out.println("Mi color favorito, es gris.");		// Imprime otro mensaje.
+		System.out.println("Ciudad de La Paz.");					// Imprime un mensaje mas.
+
+		// 6. Explora los diferentes System.X.println(); más allá de "out".
+																					// Hecho.
+
+		// 7. Utiliza varios println para imprimir una frase.
+		System.out.println("Utiliza");
+		System.out.println("varios");
+		System.out.println("println");
+		System.out.println("para");
+		System.out.println("imprimir");
+		System.out.println("una");
+		System.out.println("frase");
+
+		// 8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando simbolos).
+		System.out.println(":)");
+
+		// 9. Intenta ejecutar el programa sin el metodo main y observa el error.
+			/*
+			Error: el método principal no es HelloWorldExercises en la clase javafx.application.Application, defina el método principal del siguiente modo:
+   		public static void main(String[] args)
+			 */
+
+		// 10. Intenta cambiar el nombre del archivo a uno diferente del de la clase y compilalo. Que pasa?
+			/*
+			 * Error: no se ha encontrado o cargado la clase principal HelloWorldExercises
+				Causado por: java.lang.ClassNotFoundException: HelloWorldExercises
+			 */
+	}
+}
